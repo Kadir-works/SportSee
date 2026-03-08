6059a85756cb9621885887a577cb8ff9c95a4ffa
Kadir-works <157816175+Kadir-works@users.noreply.github.com>
2026-03-08 12:52:53 +0100

    Set USE_MOCK to true
    Changed USE_MOCK to true for local development.

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-export const USE_MOCK = false; // <-- passe à false quand ton backend tourne
+export const USE_MOCK = true; // <-- passe à false quand ton backend tourne
 
 export const API_BASE_URL = "http://localhost:3000";
